f217e7fc1fef0d7657310ab65d0d352784652de7
Kiryl Yermalovich <ts.underawater@gmail.com>
2022-03-15 02:04:22 +0300

    add: added slider and cards recipe

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,48 @@
+import { useEffect, useState } from "react";
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
+
 function Popular() {
+  const [popular, setPopular] = useState([]);
+
+  const getPopular = async () => {
+    const recipe = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`);
+    const data = await recipe.json();
+    setPopular(data.recipes)
+  };
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
   return (
-    <div>Popular</div>
+    <div>
+      <div className="recipe__meals">
+        <h3>
+          Popular Picks
+        </h3>
+        <Splide options={{
+          perPage: 4,
+          arrows: false,
+          pagination: false,
+          drag: 'free',
+          gap: '5rem'
+        }}>
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.title}>
+                <div className="recipe__meals-card">
+                  <p>
+                    {recipe.title}
+                  </p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </div>
+              </SplideSlide>
+            )
+          })};
+        </Splide>
+      </div>
+    </div>
   )
 }
 
